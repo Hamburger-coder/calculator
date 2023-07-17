cb5f952b6b5df7b0a48d35d61833cc9e64b6fc9c
hamburger-coder <joesmast@gmail.com>
2023-07-17 17:25:19 -0400

    Added variables, num1, op, and num2, and the functions operate, add, subtract, multiply, divide.

---
+++ script.js
@@ -0,0 +1,27 @@
+let num1;
+let op;
+let num2;
+
+function operate() {
+  
+}
+
+function add() {
+  console.log(1 + 1);
+}
+
+function subtract() {
+  console.log(1 - 1);
+}
+
+function multiply() {
+  console.log(1 * 1);
+}
+function divide() {
+  console.log(1 / 1);
+}
+
+add();
+subtract();
+multiply();
+divide();
