def0e07de5e201ac15c8921c81fb20c6d5645ab8
hamburger-coder <joesmast@gmail.com>
2023-07-19 16:42:18 -0400

    Added buttons for all digits and also for clear and display, added variables for all of them and got them with get element by id, also added a log in my readme file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calculator
+# July 19th
+If I manage to finish this project i would like to personally thank my brain for surpassing my imaginations, and also stack overflow for always being there for me.
--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let num1;
+let opnum1;
 let op;
-let num2;
+let opnum2;
+let displayVal = document.getElementById("#display");
+let num1 = document.getElementById("#num1");
+let num2 = document.getElementById("#num2");
+let num3 = document.getElementById("#num3");
+let num4 = document.getElementById("#num4");
+let num5 = document.getElementById("#num5");
+let num6 = document.getElementById("#num6");
+let num7 = document.getElementById("#num7");
+let num8 = document.getElementById("#num8");
+let num9 = document.getElementById("#num9");
+
+
+
+
 
 function operate() {
   
