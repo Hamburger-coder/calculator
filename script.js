877896c12fce3e6ebace5765cdf92627f9871734
hamburger-coder <joesmast@gmail.com>
2023-07-24 13:11:44 -0400

    Made where the calculator works now with 2 numbers and any operation

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+let input = "";
 let opnum1;
 let op;
 let opnum2;
-let displayVal = document.querySelector("#display").innerHTML = "";
+let displayVal = (document.querySelector("#display").innerHTML = "");
 let num1 = document.querySelector("#num1");
 let num2 = document.querySelector("#num2");
 let num3 = document.querySelector("#num3");
@@ -11,62 +12,129 @@ let num6 = document.querySelector("#num6");
 let num7 = document.querySelector("#num7");
 let num8 = document.querySelector("#num8");
 let num9 = document.querySelector("#num9");
+let num0 = document.querySelector("#num0");
+let clear = document.querySelector("#clear");
+let plus = document.querySelector("#plus");
+let minus = document.querySelector("#minus");
+let multiplication = document.querySelector("#multiplication");
+let division = document.querySelector("#division");
+let equals = document.querySelector("#equals");
 
+function operate(op, num1, num2) {
+  if (op === "+") {
+    return add(num1, num2);
+  }
 
-function operate() {
-  
+  if (op === "-") {
+    return subtract(num1, num2);
+  }
+
+  if (op === "*") {
+    return multiply(num1, num2);
+  }
+
+  if (op === "/") {
+    return divide(num1, num2);
+  }
 }
 
-function add() {
-  console.log(1 + 1);
+function add(a, b) {
+  return a + b;
 }
 
-function subtract() {
-  console.log(1 - 1);
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply() {
-  console.log(1 * 1);
+function multiply(a, b) {
+  return a * b;
 }
-function divide() {
-  console.log(1 / 1);
+function divide(a, b) {
+  return a / b;
 }
 
-num1.addEventListener("click", ()=> {
-  displayVal = document.querySelector("#display").innerHTML += "1";
-})
+function display(num) {
+  displayVal = document.querySelector("#display").innerHTML += num;
+  input += num;
+}
 
-num2.addEventListener("click", ()=> {
-  displayVal = document.querySelector("#display").innerHTML += "2";
-})
+num1.addEventListener("click", () => {
+  display(1);
+});
 
-num3.addEventListener("click", ()=> {
-  displayVal = document.querySelector("#display").innerHTML += "3";
-})
+num2.addEventListener("click", () => {
+  display(2);
+});
 
-num4.addEventListener("click", ()=> {
-  displayVal = document.querySelector("#display").innerHTML += "4";
-})
+num3.addEventListener("click", () => {
+  display(3);
+});
 
-num5.addEventListener("click", ()=> {
-  displayVal = document.querySelector("#display").innerHTML += "5";
-})
+num4.addEventListener("click", () => {
+  display(4);
+});
 
-num6.addEventListener("click", ()=> {
-  displayVal = document.querySelector("#display").innerHTML += "6";
-})
+num5.addEventListener("click", () => {
+  display(5);
+});
 
-num7.addEventListener("click", ()=> {
-  displayVal = document.querySelector("#display").innerHTML += "7";
-})
+num6.addEventListener("click", () => {
+  display(6);
+});
 
-num8.addEventListener("click", ()=> {
-  displayVal = document.querySelector("#display").innerHTML += "8";
-})
+num7.addEventListener("click", () => {
+  display(7);
+});
 
-num9.addEventListener("click", ()=> {
-  displayVal = document.querySelector("#display").innerHTML += "9";
-})
+num8.addEventListener("click", () => {
+  display(8);
+});
+
+num9.addEventListener("click", () => {
+  display(9);
+});
+
+num0.addEventListener("click", () => {
+  display(0);
+});
+
+clear.addEventListener("click", () => {
+  displayVal = document.querySelector("#display").innerHTML = "";
+});
+
+plus.addEventListener("click", () => {
+  opnum1 = Number(input);
+  display("+");
+  input = "";
+  op = "+";
+});
+
+minus.addEventListener("click", () => {
+  opnum1 = Number(input);
+  display("-");
+  input = "";
+  op = "-";
+});
+
+multiplication.addEventListener("click", () => {
+  opnum1 = Number(input);
+  display("*");
+  input = "";
+  op = "*";
+});
+
+division.addEventListener("click", () => {
+  opnum1 = Number(input);
+  display("/");
+  input = "";
+  op = "/";
+});
+
+equals.addEventListener("click", () => {
+  opnum2 = Number(input);
+  let result = operate(op, opnum1, opnum2);
+  displayVal = document.querySelector("#display").innerHTML = result;
+});
 
 add();
 subtract();
